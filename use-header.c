b35d93a4c6435392f550e9845af9e751a991b378
s-zil <siam.zilna@gmail.com>
2026-07-03 16:14:26 +0530

    Update use-header.c

--- use-header.c
+++ use-header.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "thugil.h"
 
-int main() {
+int main(void) {
 	printf("\vTami Preprocessor Header File hi e \nC compiler adia kigelh ahi: \n%s \n\v", LEIGOU);
 	printf("LEIGOU ban ah, macro ulitity tha khat SUAHLAM aki belap ta hi %s\n", SUAHLAM);
 	return 0; 
