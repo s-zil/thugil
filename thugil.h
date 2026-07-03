e7fbfa01dbd0360331b6ff3944b56ac2a02eaf06
s-zil <siam.zilna@gmail.com>
2026-07-03 15:58:03 +0530

    Add files via upload
    A new macro utility called SUAHLAM  is added to the older macro utility LEIGOU

---
+++ thugil.h
@@ -0,0 +1,55 @@
+#ifndef THUGIL_H
+#define THUGIL_H
+																		
+#define SUAHLAM "\n\nSUAHLAM MIPILTE \n"										\
+		"Tam te nunlui thu ahi ta, lunglen a kai thou hi. \n"						\
+		"Ka khualzin kisia vawt sing, nanleh silmah khat hing gen vang: \n"			\
+		"Pianna maw Shina – bang pen mu ding bep a zinkhia ka hi uai? \n"			\
+		"Kichian tah in pienna ahia, lungawp kai na ding bangma a um sih. \n"			\
+		"Pianna leh Shina ka melmu ngai ta, \n"									\
+		"Amau gel a kibang lou in ka na geel zing hi.  \n"						\
+		"Pianna in ling bang hing dou a, \n"									\
+		"Kou din Shina gu a bang hi. \n"										\
+		"Kumpi lenghuang sung a inn nop tah te ah ka kile kia ua, \n"				\
+		"Nanleh hun leh kumte ana kikhel ta, \n"								\
+		"Tam mun ah nun nop tah in ki khosa thei ta sih va, \n"					\
+		"A pasian lui te uh tukip tinten mel theilou hon lai ah \n"					\
+		"Khatvei shi kia ding ken ka teel zaw hi. \n\n"							
+		
+		/* No back slash in the last line of a utility macro and 
+		no back slash before defining a new utlity macro 
+		
+		Macro Broken can not be broken by a Blank Line (empty line); 
+		so add trailing backslash (\) at the end of the line 
+		(NOT the beginning) to prevent compilation error  */
+																		
+																		
+#define LEIGOU "\n\nSEMGIM GAHSUO LEIGOU \n"										\
+		"Mi koi in zong mihing tha ama, ganhing tha ama, \n"						\
+		"ahilou leh thadawn thei khawl zang a, \n" 								\
+		"zuun lou gam-le-lei tang beh khat a puah a, \n" 							\
+		"a manphatna a pun sah leh, \n" 										\
+		"koima’n ama gim leh tawl gah a thawn in a suuh thei sih ding hi. \n" 		\
+		"Adiah in tuilou (zaang lou), thei gam, sing gam, \n" 						\
+		"huan chi tuamtuam te leh \n" 										\
+		"mual pang kilaizang (terrace hillside) chite bawl na di’n tha a nem a, \n" 	\
+		"na semtu gimgah zatat tah a kepbit ding khu dan leh dun dih tah ahi. \n" 		\
+								"\t\t\v David Vumlallian Zou, 2 July 2026\n\n" 	\
+																		\
+																			\
+		"English ham a lehna: \n"											\
+		"If a person improves a patch of land or reclaims it \n" 					\
+		"from wasteland with human labour, animal labour, or labour-saving tools, \n" 	\
+		"none can deprive that person from the fruits of his or her labour. \n" 		\
+		"The starting of new paddy fields, horticulture, timber plantation, gardens \n" \
+		"and hillside terrace are labour-intensive; \n" 							\
+		"a just law requires the protection of the fruits of own’s labour.\n" 
+																					
+#endif
+																												
+		/* You must add a backslash (\) at the very end of each line 			
+		to tell the preprocessor that the macro continues on the next line.*/
+
+																			
+																			
+																			
