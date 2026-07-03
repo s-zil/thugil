dcbee15b03af3efd4f93e70f99df60f54797cc5d
s-zil <siam.zilna@gmail.com>
2026-07-03 16:01:46 +0530

    Update use-header.c

--- use-header.c
+++ use-header.c
@@ -3,6 +3,7 @@
 
 int main() {
 	printf("\vTami Preprocessor Header File hi e \nC compiler adia kigelh ahi: \n%s \n\v", LEIGOU);
+	printf("LEIGOU ban ah, macro ulitity tha khat SUAHLAM aki belap ta hi %s\n", SUAHLAM);
 	return 0; 
 	
 }
